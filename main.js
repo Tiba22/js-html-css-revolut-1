8635bce1a475ea6c7a8b2ed9e3c5b55a78939f7b
gabriele <sozziga@gmail.com>
2021-03-14 13:13:45 +0100

    Completato, migliorato e sistemato
    Completato

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
-  var hoverMenu = $(".selected")
+  var features = $(".features")
+  var plans =$(".plans")
+  var about = $(".about")
+  var help = $(".help")
 
-  hoverMenu.mouseenter(function(){
-    $(this).parents().find(">.menu").toggleClass("active");
+  features.mouseenter(function(){
+    $(this).parents().find(".features-content").addClass("active");
+  })
+
+  features.mouseleave(function(){
+    $(this).parents().find(".features-content").removeClass("active");
+  })
+
+  plans.mouseenter(function(){
+    $(this).parents().find(".plans-content").addClass("active");
+  })
+
+  plans.mouseleave(function(){
+    $(this).parents().find(".plans-content").removeClass("active");
+  })
+
+  about.mouseenter(function(){
+    $(this).parents().find(".about-content").addClass("active");
+  })
+
+  about.mouseleave(function(){
+    $(this).parents().find(".about-content").removeClass("active");
+  })
+
+  help.mouseenter(function(){
+    $(this).parents().find(".help-content").addClass("active");
+  })
+
+  help.mouseleave(function(){
+    $(this).parents().find(".help-content").removeClass("active");
   })
 
 })
